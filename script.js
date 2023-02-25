c21796d40e3201c4402deacb64b3f82e067c3840
Sinchana1009 <123719770+Sinchana1009@users.noreply.github.com>
2023-02-25 08:41:16 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+var typed = new Typed(".multiple-text",{
+   strings:[" and a web developer","and a aurdino guy"," and a ai ml student"],
+   typeSpeed:100,
+   backSpeed:100,
+   backDelay:1000,
+   loop:true
+}
+)
